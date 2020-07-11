db7bbbaf00d84ea3526b559c941a32a2e5b17aa5
21lucasmessias <21.lucasmessias@gmail.com>
2020-07-11 20:22:00 -0300

    first commit: add a simple package structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name             = 'bot-tw2',
+   version          = '0.0.0',
+   description      = 'A bot for Tribal Wars 2',
+   packages         = ['rash_bot_tw2'],
+   install_requires = [],
+)
