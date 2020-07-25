f07f8efffebce72f5a79ca32c9fe88262532e68b
21lucasmessias <21.lucasmessias@gmail.com>
2020-07-25 17:32:35 -0300

    Correção de bug Versao 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name             = 'bot-tw2',
-   version          = '1.0.0',
+   version          = '1.1.0',
    description      = 'A bot for Tribal Wars 2',
    packages         = ['rash_bot_tw2'],
    install_requires = [
